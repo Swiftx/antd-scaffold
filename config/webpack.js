f581882bbd002811e56247b91ebe4b525863c2b4
胡永强 <odaytudio@gmail.com>
2017-11-14 20:27:30 +0800

    调整为jsx格式

--- config/webpack.js
+++ config/webpack.js
@@ -1,14 +1,14 @@
 import path from 'path';
 export default {
     devtool : 'eval-source-map',                // 生成站点地图
-    entry :  path.resolve("../src/index.js"),   // 唯一入口文件
+    entry :  path.resolve("../src/index.jsx"),   // 唯一入口文件
     output : {
         path : path.resolve("../dist"),         // 打包后的文件存放的地方
         filename : "bundle.js"                  // 打包后输出文件的文件名
     },
     module: {
         loaders: [{
-            test: /\.js$/,
+            test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
         },{
